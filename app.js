d7f80bde49bb5811cfdd7cd704113fd2dea29383
Shus13 <sushitkarki234@gmail.com>
2026-07-05 22:29:36 +0545

    final change

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://mern-job-portal-website-frontend.vercel.app/",
+      "https://mern-job-portal-website-frontend.vercel.app",
     ],
   }),
 );
