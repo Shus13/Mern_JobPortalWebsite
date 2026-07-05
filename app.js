868c89c83e55d5476b2c51619438d25248918024
Shus13 <sushitkarki234@gmail.com>
2026-07-05 21:51:53 +0545

    update vercel link

--- app.js
+++ app.js
@@ -3,30 +3,36 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 require("./model");
 const path = require("path");
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const cors = require("cors");
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mern-job-portal-website-frontend.vercel.app/",
+    ],
+  }),
+);
 
 const { connectDb } = require("./database/dbconfig");
 connectDb();
 const jwt = require("jsonwebtoken");
 
-const userRouter = require ("./routes/userRoute")
+const userRouter = require("./routes/userRoute");
 app.use("/api/auth", userRouter);
 
-const jobRoute = require("./routes/jobRoute")
-app.use("/api/jobs", jobRoute)
+const jobRoute = require("./routes/jobRoute");
+app.use("/api/jobs", jobRoute);
 
-const applicationRoute = require("./routes/applicationRoute")
-app.use("/api/app", applicationRoute)
+const applicationRoute = require("./routes/applicationRoute");
+app.use("/api/app", applicationRoute);
 
-
-port = process.env.Port
+port = process.env.Port;
 app.listen(port, () => {
   console.log("Server is running on port", port);
 });
